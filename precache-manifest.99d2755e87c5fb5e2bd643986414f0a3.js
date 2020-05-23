12436fccd85d3d5f743c9fab67dc5d5dff69415d
andrey post <postoliuk.av@gmail.com>
2020-05-23 12:26:57 +0300

    Updates

---
+++ precache-manifest.99d2755e87c5fb5e2bd643986414f0a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952d3ab49fc2a5076bf8a56e5bf8c917",
+    "url": "/edicasoft-t-t-r/index.html"
+  },
+  {
+    "revision": "beb4cbe3ef79e89a9706",
+    "url": "/edicasoft-t-t-r/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffb1e04bbfb418cf8b90",
+    "url": "/edicasoft-t-t-r/static/js/2.086cfe31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/edicasoft-t-t-r/static/js/2.086cfe31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb4cbe3ef79e89a9706",
+    "url": "/edicasoft-t-t-r/static/js/main.285d4574.chunk.js"
+  },
+  {
+    "revision": "8f043531e7e79e2c5c54",
+    "url": "/edicasoft-t-t-r/static/js/runtime-main.e1a89e73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/edicasoft-t-t-r/static/media/logo.5d5d9eef.svg"
+  }
+]);
